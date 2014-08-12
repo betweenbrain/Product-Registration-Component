c3db5e5a1d38fe88a38cdec5a77b9a7a72183ee5
Matt Thomas <matt@betweenbrain.com>
2014-08-12 11:47:39 -0400

    Add server-side validation checks

--- site/controllers/registration.php
+++ site/controllers/registration.php
@@ -31,7 +31,51 @@ class RegistrationControllerRegistration extends RegistrationController
 		$data = JFactory::getApplication()->input->get('jform', array(), 'array');
 
 		// Initialise variables.
+		$app   = JFactory::getApplication();
 		$model = $this->getModel('register', 'RegistrationModel');
+
+		// Validate the posted data.
+		$form = $model->getForm();
+		if (!$form)
+		{
+			JError::raiseError(500, $model->getError());
+
+			return false;
+		}
+
+		// Validate the posted data.
+		$data = $model->validate($form, $data);
+
+		// Check for errors.
+		if ($data === false)
+		{
+			// Get the validation messages.
+			$errors = $model->getErrors();
+
+			// Push up to three validation messages out to the user.
+			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++)
+			{
+				if ($errors[$i] instanceof Exception)
+				{
+					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
+				}
+				else
+				{
+					$app->enqueueMessage($errors[$i], 'warning');
+				}
+			}
+
+			$input = $app->input;
+			$jform = $input->get('jform', array(), 'ARRAY');
+
+			// Save the data in the session.
+			$app->setUserState('com_registration.edit.registration.data', $jform, array());
+
+			// Redirect back to the edit screen.
+			$this->setRedirect(JRoute::_('index.php?option=com_registration', false));
+
+			return false;
+		}
 
 		// Now update the loaded data to the database via a function in the model
 		$registration = $model->submit($data);
